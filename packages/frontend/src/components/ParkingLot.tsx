ec29e581009cfe53fd535bebf3a69a4195d0e477
egenerse <egenerse@gmail.com>
2025-08-10 13:32:20 +0200

    feat: add icons for parking lots

--- packages/frontend/src/components/ParkingLot.tsx
+++ packages/frontend/src/components/ParkingLot.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ParkingIcon from "./icons/ParkingIcon";
+import ChargerIcon from "./icons/ChargerIcon";
 
 interface Props {
   id: number;
@@ -17,27 +19,35 @@ export const ParkingLot: React.FC<Props> = ({
 }) => {
   return (
     <div
-      className={`h-40 w-28 bg-gray-400/60 flex flex-col items-center border rounded-lg p-4 gap-1 relative  ${
+      className={`h-40 w-28 bg-gray-100 flex flex-col items-center border-2 rounded-lg p-4 gap-1 relative ${
         !showEdit ? "justify-center" : " "
-      }`}
+      }
+      ${chargerPowerInKw ? "border-green-600" : ""}
+      `}
     >
+      {/* Parking SVG Background */}
+      {chargerPowerInKw ? (
+        <ChargerIcon className="absolute inset-0 w-full h-full opacity-40 z-0 text-green-900" />
+      ) : (
+        <ParkingIcon className="absolute inset-0 w-full h-full opacity-40 z-0" />
+      )}
+
       <div
-        className={`flex flex-col items-center gap-2 ${
+        className={`flex flex-col items-center gap-2 relative z-10 ${
           showEdit ? "opacity-50" : ""
         }`}
       >
         {chargerPowerInKw && (
-          <>
-            <p>Power</p>
-            <p>{chargerPowerInKw} kW</p>
-          </>
+          <div className="font-extrabold text-green-700 bg-white/70 rounded-lg px-1 ">
+            {chargerPowerInKw} kW
+          </div>
         )}
       </div>
 
       {showEdit && (
         <div
           onClick={() => setParkingLotPower(id)}
-          className="cursor-pointer flex flex-col gap-2 items-center justify-center absolute top-0 right-0 left-0 bottom-0 h-full w-full text-white"
+          className="cursor-pointer flex flex-col gap-2 items-center justify-center absolute top-0 right-0 left-0 bottom-0 h-full w-full text-white z-20"
         >
           <div className="bg-green-600 rounded-full w-8 h-8 flex justify-center items-center opacity-100 shadow-md text-lg font-bold">
             +
@@ -57,7 +67,7 @@ export const ParkingLot: React.FC<Props> = ({
       )}
       {chargerPowerInKw && !showEdit && (
         <div
-          className="bg-red-400 rounded-full w-8 h-8 flex justify-center items-center opacity-100 shadow-md text-lg font-bold"
+          className="bg-red-400 rounded-full w-8 h-8 flex justify-center items-center opacity-100 shadow-md text-lg font-bold z-20 relative"
           onClick={(e) => {
             e.stopPropagation();
             removeParkingLotPower(id);
